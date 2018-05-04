9bbade943455187904b27418536eec875cf91838
Sarthak Arora <123@gmail.com>
2018-05-04 09:41:46 +0530

    asssignment3.cpp

---
+++ assignment3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int Length;
+	int Breadth;
+	
+	cout<<"Enter Length ";
+	cin>>Length;
+	
+	cout<<"Enter Breadth ";
+	cin>>Breadth;
+	
+	cout<<"Area = ";
+	cout<<Length*Breadth;
+	cout<<"\n";
+	
+	cout<<"Perimeter = ";
+	cout<<2*(Length=Breadth);
+	return 0;
+}
