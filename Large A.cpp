609330759ff35c5cb7032289bf2d32c023fdf443
SarthakArora23 <38681733+SarthakArora23@users.noreply.github.com>
2018-05-04 09:46:57 +0530

    Delete Large A.cpp

--- Large A.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-int main(){
-	cout<<"                                  A";
-	cout<<"                                 A  A";
-	cout<<"                                A    A";
-	cout<<"                               A      A";
-	cout<<"                              A        A"
-}
