5d17daeb5fb9890af7845c62b074a821ea094320
Sarthak Arora <pornhub.com>
2018-05-04 09:25:19 +0530

    Calculator..cpp

---
+++ Calculator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int X;
+	int Y;
+	
+	cout<<"Enter you \"First Number\" here :";
+	cin>>X;
+	
+	cout<<"Enter your \"Second Number\" here :";
+	cin>>Y;
+	
+	cout<<"X + Y =";
+	cout<<X+Y;
+	
+	return 0;
+	
+}
--- programming blueprint.cpp
+++ programming blueprint.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main(){
-cout<<"Hello World";
+cout<<"Programming Blueprint";
+cout<<"\n";
+cout<<"This is how you use C++!";
+cout<<"\n";
+cout<<"Numbers";
+cout<<"\n"; 
+cout<<696969;
+cout<<"\n";
+cout<<"Text";
+cout<<"\n";
+cout <<"Anything";
 return 0;
 }
 
